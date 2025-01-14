ecc775d802b63c091571611a998ff7d561e8122c
sahinoor101 <sahinoor101@gmail.com>
2025-01-14 16:13:53 +0530

    working Monthly Calendar

--- calendar.js
+++ calendar.js
@@ -1,28 +1,61 @@
-const day=document.querySelectorAll(".date");
+const day = document.querySelectorAll(".date");
 console.log(day);
 const MonthName = document.querySelector(".monthName");
+let arrOfName = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULIY", "AGUST", "SEPTEMBER", "OCTOBER", "NOVERMEBR", "DECEMBER"];
 
-let firstday=6;
-let date=1;
-let arrOfName =["JANUARY" , "FEBRUARY","MARCH","APRIL","MAY","JUNE","JULIY","AGUST","SEPTEMBER" ,"OCTOBER" ,"NOVERMEBR" ,"DECEMBER" ];
-
-
-function getValue(){
-    const year=document.querySelector("#year");
-    const month=document.querySelector("#month");
-    let year1=year.value;
-    MonthName.textContent = arrOfName[month.value - 1] +"   "+ year1;
+// 1/1/1 was monday Acording to Gregorian Calendar
+function isLeap(year) {
+    return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
 }
 
-for(let i=0 ; i< 42;i++)
-{
-    if(i >=firstday && date<31)
-    {
-        day[i].textContent = date;
-        date++;
+function getFirstday(year, month) {
+    let firstday = 0;
+    for (let i = 1; i <= year; i++) {
+        if (isLeap(i)) {
+            firstday += 2;
+        }
+        else {
+            firstday++;
+        }
     }
-    else{
-        day[i].textContent ="";
-    }
+    firstday %= 7;
+    return getFirstdayOfMonth(month, year, firstday);
 }
 
+function getValue() {
+    const year = document.querySelector("#year");
+    const month = document.querySelector("#month");
+    let year1 = year.value;
+    let month1 = month.value;
+    MonthName.textContent = arrOfName[month.value - 1] + "   " + year1;
+    let value = getFirstday(year1, month1);
+    display(value, month1);
+}
+
+function getFirstdayOfMonth(month, year, firstday) {
+    // return firstday;
+    let noOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (isLeap(year)) {
+        noOfDays[1] = 29;
+    }
+    for (i = 1; i < month; i++) {
+        firstday += noOfDays[i - 1];
+    }
+    return firstday % 7;
+}
+function display(value, month) {
+    let noOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (isLeap(year)) {
+        noOfDays[1] = 29;
+    }
+    let date = 1;
+    for (let i = 0; i < 42; i++) {
+        if (i >= value && date <= noOfDays[month - 1]) {
+            day[i].textContent = date;
+            date++;
+        }
+        else {
+            day[i].textContent = "";
+        }
+    }
+}
